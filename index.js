ac49260e5977b0b393af28a7e2c27ed080c767ab
Ethan Girouard <mr.egirouard@gmail.com>
2021-07-22 20:01:06 -0400

    Read config and authenticate bot

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 const configReader = require('./ConfigReader.js')
 const discord = require('discord.js')
+
+const bot = new discord.Client()
+
+const configFile = './Config.json'
+const configOptions = {'Discord API Token':''}
+var config
+
+configReader.readOptions(configFile, configOptions, false).then((config) =>
+{
+    config = config
+    console.info('[  OK  ] Succsfully read configuration file.')
+
+    bot.login(config['Discord API Token']).then(() =>
+    {        
+        console.info('[  OK  ] Successfully logged in to Discord.')
+    }).catch((error) =>
+    {
+        console.error('[ FAIL ] Error logging in to Discord:', error)
+        process.exit(1)
+    })
+}).catch((error) =>
+{
+    console.error('[ FAIL ] Error reading configuration file:', error)
+    process.exit(1)
+})
